6562a99690802ce577a17a21f93bb7c3c2f1da02
meda840 <meda840@gmail.com>
2025-10-03 17:51:45 +0100

    added info extraction using open ai gpt 4omini

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from service.download import download_audio
 from service.convert import convert_to_wav
 from service.transcribe import transcribe_audio
 from service.transcribeAssembly import transcribe_with_assemblyai
+from service.extract_infos import extract_infos_from_text
 
 app = FastAPI()
 
@@ -19,9 +20,18 @@ def download_file(url: str, filename: str):
     # Step 3: Transcribe
     #transcript_path = transcribe_audio(filename)
     transcript_path = transcribe_with_assemblyai(filename)
+
+    # Step 4: Lire le fichier texte
+    with open(transcript_path, "r", encoding="utf-8") as f:
+        transcript_text = f.read()
+    
+    # Step 5: Envoyer à OpenAI pour extraction des infos
+    extracted_infos = extract_infos_from_text(transcript_text)
+
     return {
         "filename": filename,
         "raw_file": raw_path,
         "processed_file": wav_path,
-        "transcript_file": transcript_path
+        "transcript_file": transcript_path,
+        "extracted_infos": extracted_infos
     }
